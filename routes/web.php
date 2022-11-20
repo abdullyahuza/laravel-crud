a88a4d0b508e9beec9863c9af2c798d89c01bc09
abdullyahuza <yahuzaabdulrazak@gmail.com>
2022-11-20 07:57:15 +0100

    add a route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resource('/products', ProductController::class);
+
+Route::get('/hello',function(){
+    return view('hello');
 });
